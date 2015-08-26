11bab2ed930419a40714285efc4fc31c05e1a731
Iván R. Galicia <irodgal@gmail.com>
2015-08-26 13:19:42 +0200

    Primer commit

---
+++ pruebaGit/src/main/java/pruebaGit/pruebaGit/App.java
@@ -0,0 +1,13 @@
+package pruebaGit.pruebaGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
